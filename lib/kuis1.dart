a86872976c55355eb814f31fed03c0cfb8a78385
Trisinus123 <trisinusgl@gmail.com>
2023-09-29 18:23:51 +0700

    Kuis_1

---
+++ lib/kuis1.dart
@@ -0,0 +1,84 @@
+void main() {
+  // Tugas 1
+  print('Hello World');
+
+   // Tugas 2
+  String name = 'Trisinus Gulo';
+
+  print(name);
+  print(name);
+  print(name);
+
+  // Tugas 3
+  String firtsName = 'Trisinus';
+  final lastName = 'Gulo';
+
+  print(firtsName);
+  print(lastName);
+
+  // Tugas 4
+  final array1 = [1, 2, 3];
+  const array2 = [4, 5, 6];
+
+  array1[0] = 100;
+
+  print(array1);
+  print(array2);
+
+  // Tugas 5
+  int number1 = 100;
+  double number2 = 100.5;
+
+  print(number1);
+  print(number2);
+
+  // Tugas 6
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+
+  // Tugas 7
+  String firstName = 'Trisinus';
+  String LastName = 'Gulo';
+
+  var fullName = '$firstName ${LastName}';
+
+  print(fullName);
+
+  // Tugas 8
+  var text = 'ini \'OPTIMAL\' \$sekali';
+  print(text);
+
+  // Tugas 9
+  var name1 = firstName + lastName;
+  var name2 = 'Trisinus' 'Gulo';
+
+  print(name1);
+  print(name2);
+
+  // Tugas 10
+  var name3 = firstName + " " + lastName;
+  var name4 = 'Trisinus' 'Gulo';
+
+  print(name3);
+  print(name4);
+
+// Tugas 11
+  var kalimat_panjang = '''
+  ini
+  saya punya
+  proyek dart
+  banyak ''';
+  print(kalimat_panjang);
+}
+
+
+
+
+
+
+
+
+
